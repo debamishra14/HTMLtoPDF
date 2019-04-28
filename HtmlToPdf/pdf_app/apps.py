8645534ad3c4e7caab518ca9d54e63b899c04f25
unknown <mdebaprasad14@gmail.com>
2019-04-28 16:34:08 +0530

    initial commit

---
+++ HtmlToPdf/pdf_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PdfAppConfig(AppConfig):
+    name = 'pdf_app'
---
+++ HtmlToPdf/pdf_app/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.views.generic import View
+ 
+#importing get_template from loader
+from django.template.loader import get_template
+ 
+#import render_to_pdf from util.py 
+from .utils import render_to_pdf 
+ 
+#Creating our view, it is a class based view
+class GeneratePdf(View):
+     def get(self, request, *args, **kwargs):
+        
+        #getting the template
+        pdf = render_to_pdf('index.html')
+         
+         #rendering the template
+        return HttpResponse(pdf, content_type='application/pdf')
